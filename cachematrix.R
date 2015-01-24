b44a6531a3cc5a59781f8a060b214468b5fad68c
pternyuk <pternyuk@gmail.com>
2015-01-24 22:30:56 +0300

    Add comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,17 +1,24 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This file conrains 2 function to operate with matrix. 
+## makeCacheMatrix - create object with 4 methods. Get/Set Value of
+## Original Matrix and Get/Set Value of Inv Matrix
+## cacheSolve - return Inv Matrix of Original Matrix in makeCacheMatrix object.
+## Inv Matrix returns from cache (if it was precalculeted) or from function solve
 
-## Write a short comment describing this function
+## Create object to operate with Matrix
 
 makeCacheMatrix <- function(x = matrix()) {
   InvMatrix <- NULL
   set <- function(y) {
+    ## Save in memory Original Matrix
     OrigMatrix <<- y
     ## Destroy Invent Matrix if Orig Matrix change value
     InvMatrix <<- NULL
   }
+  ## Return Original Matrix from memory
   get <- function() OrigMatrix
-  setInv <- function(mean) InvMatrix <<- mean
+  ## Save in memory Inv Matrix
+  setInv <- function(Inv) InvMatrix <<- Inv
+  ## Return Inv Matrix from memory
   getInv <- function() InvMatrix
   
   list(set = set, get = get,
@@ -20,18 +27,24 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+## Calculate (or read from cache) and return Inv Matrix to Matrix in object x
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  ## Read Inv Matrix from object which was created by makeCacheMatrix function
   InvMatrix <- x$getInv()
-  
+  ## Check if there is Inv Matrix in object
   if(!is.null(InvMatrix)) {
     message("It's cached data!!!")
+    ## Return Inv Matrix from cache
     return(InvMatrix)
   }
+  ## Read Original Matrix from object which was created by makeCacheMatrix function
   data <- x$get()
+  ## Calculate Inv Matrix
   InvMatrix <- solve(data)
+  ## Save Inv Matrix in object which was created by makeCacheMatrix function
   x$setInv(InvMatrix)
+  ## Return Inv Matrix
   InvMatrix
 }
